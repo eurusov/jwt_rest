3e23117a3b9d20696ee7df33a95b6c636504f976
eurusov <eurusov@users.noreply.github.com>
2019-11-08 01:33:38 +0300

    Minor changes.

--- src/main/java/eurusov/jwt_rest/rest/AuthenticationRestController.java
+++ src/main/java/eurusov/jwt_rest/rest/AuthenticationRestController.java
@@ -10,15 +10,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080", maxAge = 3600)
 @AllArgsConstructor
 @RequestMapping("/api")
 public class AuthenticationRestController {
